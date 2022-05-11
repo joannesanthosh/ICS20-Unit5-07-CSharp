9fb5ca1f37ae54ca4292a6d0cb71c34938ec37fb
Joanne Santhosh <99355156+joannesanthosh@users.noreply.github.com>
2022-05-11 17:17:02 +0000

    fix main.cs

--- main.cs
+++ main.cs
@@ -9,22 +9,21 @@ class Program
 {
     public static void Main(string[] args)
     {
-       {
-        int total = 0;
-        int number;
-        // input
-        Console.WriteLine("This program adds up numbers using loops");
-        Console.WriteLine("");
-        Console.Write("Enter a positive integer: ");
-        number = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("");
-         
+      {
+      int total = 0;
+      int number;
+      // input
+      Console.WriteLine("This program adds up numbers using loops");
+      Console.WriteLine("");
+      Console.Write("Enter a positive integer: ");
+      number = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("");   
         //Process
-        for (int counter = 1; counter <= number; counter++) {
-        total += counter;
-        }
-        Console.WriteLine  ("The answer is: " + total);
-        Console.WriteLine("\nDone.");
-    }  
-  }
+      for (int counter = 1; counter <= number; counter++) {
+      total += counter;
+      }
+      Console.WriteLine ("The answer is: " + total);
+      Console.WriteLine("\nDone.");
+      }  
+    }
 }
