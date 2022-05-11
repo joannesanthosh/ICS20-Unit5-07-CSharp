2ce89bd8cc6a542f48fe4fdf5fac197dc9afd973
Joanne Santhosh <99355156+joannesanthosh@users.noreply.github.com>
2022-05-11 21:48:24 +0000

    fix main.cs

--- main.cs
+++ main.cs
@@ -10,20 +10,20 @@ class Program
     public static void Main(string[] args)
     {
       
-      int number;
-      var total = 0;
+        int number;
+        var total = 0;
         
-      // input
-      Console.WriteLine("This program adds up numbers using loops");
-      Console.WriteLine("");
-      Console.Write("Enter a positive integer: ");
-      number = Convert.ToInt32(Console.ReadLine());
-      Console.WriteLine("");   
+         // input
+        Console.WriteLine("This program adds up numbers using loops");
+        Console.WriteLine("");
+        Console.Write("Enter a positive integer: ");
+        number = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("");   
         
-      // Process
-      for (var counter = 1; counter <= number; counter++) {
-      total += counter;
-      }
-      Console.WriteLine ("The answer is: " + total);
+        // Process
+        for (var counter = 1; counter <= number; counter++) {
+        total += counter;
+        }
+        Console.WriteLine ("The answer is: " + total);
     }  
 }
