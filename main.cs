10e463646fd4a0a54f74bf6711369c449f45bdd0
Joanne Santhosh <99355156+joannesanthosh@users.noreply.github.com>
2022-05-11 22:01:16 +0000

    fix main.cs

--- main.cs
+++ main.cs
@@ -8,21 +8,19 @@ using System;
 class Program
 {
     public static void Main(string[] args)
-    { 
+    {
+
         int number;
         var total = 0;
-         // input
-        Console.WriteLine("This program adds up numbers using loops");
+
+        Console.Write("This program adds up numbers using loops");
         Console.WriteLine("");
-      
         Console.Write("Enter a positive integer: ");
         number = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("");   
-        // Process
-        for (var counter = 1; counter <= number; counter++) {
-        total += counter;
-        }
-      
+
+        for (var counter = 1; counter <= number; counter++)
+            total += counter;
+
         Console.WriteLine("The answer is: " + total);
-    }  
+    }
 }
